fff9fad6dabb4d25055322f6bdd1bf55dc8bf100
Madalina-Eleonora Gheorghe <madalinaeleonora.gheorghe@gmail.com>
2023-01-18 23:04:30 +0200

    added contact, employees and students pages content

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { TranslationServiceService } from 'src/app/services/translation-service.service';
+import * as data from '../../../assets/data.json';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  isLanguageSetSubscription: Subscription;
+  language: string;
+  DATA_VARIABLES = JSON.parse(JSON.stringify(data));
 
+  constructor(private translationService: TranslationServiceService) {}
+
+  ngOnInit() {
+    this.isLanguageSetSubscription =
+      this.translationService.isLanguageSetObservable.subscribe((result) => {
+        this.language = result;
+      });
+  }
+
+  ngOnDestroy() {
+    this.isLanguageSetSubscription.unsubscribe();
+  }
 }
--- src/app/pages/students/students.component.ts
+++ src/app/pages/students/students.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { TranslationServiceService } from 'src/app/services/translation-service.service';
+import * as data from '../../../assets/data.json';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrls: ['./students.component.scss']
+  styleUrls: ['./students.component.scss'],
 })
 export class StudentsComponent {
+  isLanguageSetSubscription: Subscription;
+  language: string;
+  DATA_VARIABLES = JSON.parse(JSON.stringify(data));
 
+  constructor(private translationService: TranslationServiceService) {}
+
+  ngOnInit() {
+    this.isLanguageSetSubscription =
+      this.translationService.isLanguageSetObservable.subscribe((result) => {
+        this.language = result;
+      });
+  }
+
+  ngOnDestroy() {
+    this.isLanguageSetSubscription.unsubscribe();
+  }
 }
